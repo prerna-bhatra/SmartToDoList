9d169293336ea8bd0fffb23f7bddeaee766f836f
prerna <bhatraprerna061@gmail.com>
2020-09-11 11:17:26 +0530

    done bi

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const ports = process.env.PORT || 3000;
+
 const User=require('./src/modeles/user')
 const Task=require('./src/modeles/task')
 const userRouter=require('./src/routers/user')
@@ -10,7 +12,6 @@ const CronJob = require('cron').CronJob;
 const {cronf}=require('./src/cronfile')
 const app=express()
 
-const port=process.env.PORT|| 3000
 
 app.use(express.json())
 app.use(userRouter)
@@ -20,7 +21,7 @@ app.use(taskRouter)
 async function main(){
     await cronf();
 }
-
+app.get('/', (req, res) => res.send('Hello Board Infinity'))
 
 new CronJob('* * * * * *', function() {
    main();
@@ -32,8 +33,8 @@ mongoose.connect(
     'mongodb+srv://prerna:Prerna123@cluster0.jokxx.mongodb.net/todolist?retryWrites=true&w=majority', { useNewUrlParser: true }
 ).then(
 
-    app.listen(3000,  ()=> {
-        console.log(`Server running at http://localhost:3000`);
+    app.listen(ports,  ()=> {
+        console.log("Server running at "+ports);
     })
 ).catch(err => {
     console.log(err);
