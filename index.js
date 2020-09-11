b1f023e2e0420e1f2f8b76e9e0b9d037563c2a2c
prerna bhatra <38524873+prerna-bhatra@users.noreply.github.com>
2020-09-11 16:20:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const ports = process.env.PORT || 3000;
 
+const User=require('./src/modeles/user')
 const Task=require('./src/modeles/task')
+const userRouter=require('./src/routers/user')
 const taskRouter=require('./src/routers/task')
 const express = require('express');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const CronJob = require('cron').CronJob;
 
 const {cronf}=require('./src/cronfile')
 const app=express()
+const router = express.Router();
 
 app.use(express.json())
-
+app.use(userRouter)
 app.use(taskRouter)
 
 
 async function main(){
     await cronf();
 }
-app.get('/', (req, res) => res.send('Hello Board Infinity'))
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/file.html'));
+    //__dirname : It will resolve to your project folder.
+  });
 
 new CronJob('* * * * * *', function() {
    main();
  }, null, true, 'Asia/Kolkata');
+
+
 
 mongoose.connect(
     'mongodb+srv://prerna:Prerna123@cluster0.jokxx.mongodb.net/todolist?retryWrites=true&w=majority', { useNewUrlParser: true }
